cc9e4be206dd215342a54d30e009f0fecec62c84
viet <viettangle12@gmail.com>
2018-08-12 19:50:12 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # renta2z-demo
+## node with express and angular
+- what angular version: lastest angular version
+## Deploy to heroku
+- set up your heroku project to deploy using GitHub
+- package.json (located on root folder) contains all node dependencies
+- the scripts section (postinstall) allows npm to install latest packages and build the UI so that the ui will have a dist folder using ng cli from "ui/package.json"
+- package.json (ui/package.json) need to have @angular-devkit/build-angular, @angular/cli, and @angular/compiler-cli as depedencies as well as under devDependencies
--- server/index.js
+++ server/index.js
@@ -1,24 +1,3 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-// // Serve only the static files form the dist directory
-// app.use(express.static(path.resolve(__dirname, '../ui/dist')));
-
-// app.get('/*', function(req,res) {
-    
-//   res.sendFile(path.resolve(__dirname, '../ui/dist', 'index.html'));
-// });
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
-
-
-
-
-
 'use strict'
 const express = require('express');
 // const request = require('request');
